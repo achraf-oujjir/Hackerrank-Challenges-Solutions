052b8fd66a1f63d4362053fed7716233ded2979c
Achraf OUJJIR <oujjir.achraf@gmail.com>
2024-08-19 03:38:38 +0100

    corrected gcd strings

--- leetcode/greatest-common-divisor-of-strings.py
+++ leetcode/greatest-common-divisor-of-strings.py
@@ -1,12 +1,14 @@
 def gcdOfStrings(str1: str, str2: str) -> str:
-    small = str2 if len(str2) < len(str1) else str1
-    big = str1 if len(str1) > len(str2) else str2
-    gcd = small
-    while gcd != '':
-        if len(big) % len(gcd) == 0:
-            k = len(big) // len(gcd)
-            if big == k*gcd: return gcd
-        gcd = gcd[:-1]
+    len1, len2 = len(str1), len(str2)
+
+    def valid(k):
+        if len1 % k or len2 % k: return False
+        n1, n2 = len1 // k, len2 // k
+        base = str1[:k]
+        return str1 == n1 * base and str2 == n2 * base
+    
+    for i in range(min(len(str1), len(str2)), 0, -1):
+        if valid(i): return str1[:i]
     return ''
 
 
