c8b2420adabdd69b2873733f4aba21a0902b2435
Achraf OUJJIR <oujjir.achraf@gmail.com>
2024-08-21 00:38:09 +0100

    optimized can place flowers

--- leetcode/can-place-fowers.py
+++ leetcode/can-place-fowers.py
@@ -36,6 +36,19 @@ def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
     print(valid_plots)
     return valid_plots >= n
 
+def optimizedWay(flowerbed: list[int], n: int) -> bool:
+    count = 0
+    for i in range(len(flowerbed)):
+        if flowerbed[i] == 0:
+            empty_left = (i==0) or (flowerbed[i-1] == 0)
+            empty_right = (i==len(flowerbed)-1) or (flowerbed[i+1] == 0)
+            if empty_left and empty_right:
+                flowerbed[i] = 1
+                count += 1
+    return count >= n
+    
+    
+
 flowerbed = [1,0,0,0,1]
 n = 2
 print(canPlaceFlowers(flowerbed, n))
