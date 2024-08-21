6af4e3eb056f9b4ea7b5d530d41188416549b4ff
Achraf OUJJIR <oujjir.achraf@gmail.com>
2024-08-21 02:38:46 +0100

    optimized reverse vowels

--- leetcode/reverse-vowels.py
+++ leetcode/reverse-vowels.py
@@ -8,5 +8,25 @@ def reverseVowels(s: str) -> str:
         result[t[0]] = v[len(v) - j - 1][1]
     return ''.join(result)
 
+def optimizedReverseVowels(s: str) -> str:
+    word = list(s)
+    left = 0
+    right = len(word) - 1
+    vowels = "AaEeIiOoUu"
+    
+    while left < right:
+        while left < right and word[left] not in vowels:
+            left += 1
+        
+        while left < right and word[right] not in vowels:
+            end -= 1
+        
+        word[left], word[right] = word[right], word[left]
+        left += 1
+        right -= 1
+    return "".join(word)
+    
+    
+
 s = "hello"
-print(reverseVowels(s))
+print(optimizedReverseVowels(s))
