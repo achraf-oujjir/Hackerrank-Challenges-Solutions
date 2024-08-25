b1488eab71381f0cedfc2970582c54f3f156c24e
Achraf OUJJIR <oujjir.achraf@gmail.com>
2024-08-25 22:25:34 +0100

    max sub array

--- neetcode-75-blind/53-maximum-subarray.py
+++ neetcode-75-blind/53-maximum-subarray.py
@@ -1,10 +1,10 @@
 def maxSubArray(nums: list[int]) -> int:
-    maxSum, curSum = nums[0], 0
+    maxSub, curSum = nums[0], 0
     for n in nums:
         if curSum < 0: curSum = 0
         curSum += n
-        maxSum = max(maxSum, curSum)
-    return maxSum
+        maxSub = max(maxSub, curSum)
+    return maxSub
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 print(maxSubArray(nums))
